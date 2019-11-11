8d4b953e871ed7f77d7fd967072038bf62094a1b
Ian Cook Westgate <iancookwestgate@gmail.com>
2019-11-11 09:22:57 -0800

    merge static HTML with WordPress project.

--- app/public/wp-content/themes/Unique-Blog-Theme/functions.php
+++ app/public/wp-content/themes/Unique-Blog-Theme/functions.php
@@ -1,7 +1,10 @@
 <?php
 
 function blog_files() {
-  wp_enqueue_style('blog_main_styles', get_stylesheet_uri());
+  wp_enqueue_script('main-blog-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
+  wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css?ver=5.2.2');
+  wp_enqueue_style('blog_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
 add_action('wp_enqueue_scripts', 'blog_files');
