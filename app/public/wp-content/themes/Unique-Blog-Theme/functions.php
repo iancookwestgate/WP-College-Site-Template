19bef00dcb5e12b48f57bde284aae848209fe207
Ian Cook Westgate <iancookwestgate@gmail.com>
2019-11-11 09:44:51 -0800

    insert WordPress functions for links.

--- app/public/wp-content/themes/Unique-Blog-Theme/functions.php
+++ app/public/wp-content/themes/Unique-Blog-Theme/functions.php
@@ -8,3 +8,9 @@ function blog_files() {
 }
 
 add_action('wp_enqueue_scripts', 'blog_files');
+
+function blog_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'blog_features');
